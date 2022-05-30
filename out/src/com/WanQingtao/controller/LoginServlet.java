508f08752bf0f7c3c1db6616647e896fe51ea2c9
black-beetle-6 <2037255630@qq.com>
2022-05-30 22:40:36 +0800

    lab3

--- out/src/com/WanQingtao/controller/LoginServlet.java
+++ out/src/com/WanQingtao/controller/LoginServlet.java
@@ -77,5 +77,45 @@ public class LoginServlet extends HttpServlet {
         }
 
 
+
+
+
+
+//        try {
+//
+//            //lets change code to make MVC
+//
+//            //TODO 4: VALIDATE USER - SELEECT * FROM USERTABLE WHERE USERNAME='XXX'
+//            // AND PASSWORD='YYY'
+//            String sql="select id,username,password,email,gender,birthdate from usertable where username='"+username+"' and password='"+password+"'";
+//
+//            ResultSet rs =con.createStatement().executeQuery(sql);
+//            if (rs.next()){
+//                //week 5 code
+//                //out.print("Login Successful!!!");
+//                //out.print("Welcome"+username);
+//                //get from rs and set into resquest attribute
+//
+//                request.setAttribute("id",rs.getInt("id"));
+//                request.setAttribute("username",rs.getString("username"));
+//                request.setAttribute("password",rs.getString("password"));
+//                request.setAttribute("email",rs.getString("email"));
+//                request.setAttribute("gender",rs.getString("gender"));
+//                request.setAttribute("birthDate",rs.getString("birthdate"));
+//                //forward to userInfo.jsp
+//                request.getRequestDispatcher("userInfo.jsp").forward(request,response);
+//
+//            }else{
+//                //out.print("Username or password Error!!!");
+//                request.setAttribute("message","Username or Password Error!!!");
+//                request.getRequestDispatcher("login.jsp").forward(request,response);
+//
+//            }//end of else
+//
+//
+//        } catch (SQLException throwables) {
+//            throwables.printStackTrace();
+//        }
+
     }
 }
--- out/src/com/WanQingtao/controller/RegisterServlet.java
+++ out/src/com/WanQingtao/controller/RegisterServlet.java
@@ -14,6 +14,20 @@ public class RegisterServlet extends HttpServlet {
     @Override
     public void init() throws ServletException {
         super.init();
+//4. Get all 4 context init parameters within RegisterServletinit()
+       /* ServletContext context=getServletContext();
+        String driver=context.getInitParameter("driver");//<param-name>driver</param-name>
+        String url=context.getInitParameter("url");
+        String username=context.getInitParameter("username");
+        String password=context.getInitParameter("password");
+        //5.Create database connection within init()
+        try {
+            Class.forName(driver);
+            con = DriverManager.getConnection(url, username, password);
+            System.out.println("init()--> " + con);// ok(java code)- ok(use config-in web.xml) -- ok -use( @webservlet )--> (use context) ok-- use mysql ( no need to change in java code )
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }*/
 
 
         con = (Connection) getServletContext().getAttribute("con");//name of attibute
@@ -48,12 +62,53 @@ public class RegisterServlet extends HttpServlet {
             System.out.println("n-->" + n);//== 1 success--insert
 
 
+            //7. seleect all rows from user table
+          //  sql="select id,username,password,email,gender,birthdate from usertable ";
+           // ResultSet rs= st.executeQuery(sql);
+            //PrintWriter out=response.getWriter();
+
+          /*  out.println("<html><title></title><body><table border=1><tr>");
+            out.println("<td>Username</td><td>password</td><td>Email</td><td>Gender</td><td>Birthday</td><tr/>");
+          while(rs.next()){
+//get one by one
+                out.println("<tr>");
+                out.println("<td>"+rs.getString("username")+"</td>");
+                out.println("<td>"+rs.getString("password")+"</td>");
+                out.println("<td>"+rs.getString("email")+"</td>");
+                out.println("<td>"+rs.getString("gender")+"</td>");
+                out.println("<td>"+rs.getString("birthdate")+"</td>");
+
+                out.println("</tr>");
+
+
+            }
+            out.println("</table></body></html>");*/
+
+            //use request attribute
+            //set rs into request attribute
+            //request.setAttribute("rsname",rs); //name - string , value - any type (object)
+
+            //request.getRequestDispatcher("userList.jsp").forward(request,response);
+
+           //week6
+            //after register a new user - user can login
             response.sendRedirect("login");
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
+
+        //print all data-- write into response
+         /*PrintWriter out=response.getWriter();
+         out.println("UserName : "+username);
+        out.println("Password : "+password);
+        out.println("email : "+email);
+        out.println("gender : "+gender);
+        out.println("BirthDate : "+birthDate);
+        out.println("UserName : "+username);*/
+
+        //
 
     }
 }
